92055f2df43a33469d80bf6582f994b52e1fa954
Todd Tan <todd.tan@capacity.com>
2023-08-17 21:46:38 -0500

    Some style cleanup

--- src/components/CreateGifButton.js
+++ src/components/CreateGifButton.js
@@ -1,15 +1,9 @@
 import React from "react";
 
 const CreateGifButton = ({ onClick }) => (
-  <div className="container">
-    <div className="row row-cols-4">
-      <div className="col">
-        <button className="btn btn-primary" onClick={onClick} type="button">
-          Create GIF
-        </button>
-      </div>
-    </div>
-  </div>
+  <button className="btn btn-primary" onClick={onClick} type="button">
+    Create GIF
+  </button>
 );
 
 export default CreateGifButton;
--- src/components/UploadedImage.js
+++ src/components/UploadedImage.js
@@ -1,4 +1,4 @@
-import React, { forwardRef, useRef } from "react";
+import React, { forwardRef } from "react";
 
 const UploadedImage = forwardRef((props, ref) => {
   return (
